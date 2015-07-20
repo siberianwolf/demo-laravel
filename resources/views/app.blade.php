c327997e95204e946511683a075b3cb79220b748
vlsoprun <vlsoprun@yandex.ru>
2015-07-20 17:18:35 +0300

    Update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,9 +9,10 @@
 <link rel="icon"             href="{{ asset('favicon.png') }}">
 <link rel="shortcut icon"    href="{{ asset('shortcut.png') }}">
 <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
-<link rel="stylesheet"       href="//fonts.googleapis.com/css?family=Open+Sans:400,700,300,600,800&subset=latin,cyrillic">
-<link rel="stylesheet"       href="//cdn.jsdelivr.net/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet"       href="//cdn.jsdelivr.net/fontawesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet"       href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600,800&subset=latin,cyrillic">
+<link rel="stylesheet"       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+{{--<link rel="stylesheet"       href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/sandstone/bootstrap.min.css">--}}
+<link rel="stylesheet"       href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet"       href="{{ asset('css/app.css') }}">
 
 <main id="main" class="container">
@@ -21,25 +22,31 @@
     </header>
     <hr>
     <div class="row">
-        <section class="col-sm-8">
+        <section class="col-sm-9">
             @yield('content')
         </section>
-        <aside id="sidebar" class="col-sm-3 col-sm-offset-1">
-            <ul class="list-unstyled">
-                @foreach(App\Category::all() as $category)
-                    <li>{!! link_to_route('category.show', $category->name, $category) !!}</li>
-                @endforeach
-            </ul>
-            <hr>
-            <p class="h4">Последние 5 постов</p>
-            <hr>
-            <ul class="list-unstyled">
-                @foreach(App\Post::orderBy('created_at', 'desc')->take(5)->get() as $post)
-                    <li>{!! link_to_route('post.show', $post->name, $post) !!}</li>
-                @endforeach
-            </ul>
-            <hr>
-            {!! link_to_route('post.create', trans('post.create'), [], ['class' => 'btn btn-info']) !!}
+        <aside id="sidebar" class="col-sm-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="panel-title">Categories</div>
+                </div>
+                <ul class="list-group">
+                    @foreach(App\Category::all() as $category)
+                        <li class="list-group-item">{!! link_to_route('category.show', $category->name, $category) !!}</li>
+                    @endforeach
+                </ul>
+                <div class="panel-heading">
+                    <div class="panel-title">Последние 5 постов</div>
+                </div>
+                <ul class="list-group">
+                    @foreach(App\Post::orderBy('created_at', 'desc')->take(5)->get() as $post)
+                        <li class="list-group-item">{!! link_to_route('post.show', $post->name, $post) !!}</li>
+                    @endforeach
+                </ul>
+                <div class="panel-footer text-center">
+                    {!! link_to_route('post.create', trans('post.create'), [], ['class' => 'btn btn-info']) !!}
+                </div>
+            </div>
         </aside>
     </div>
 </main>
--- resources/views/post/item.blade.php
+++ resources/views/post/item.blade.php
@@ -1,20 +1,22 @@
-<article class="post-item col-xs-12 row">
-    <div class="col-xs-4 row">
-        <a href="{{ route('post.show', $post) }}">
-            <img class="img-thumbnail" src="https://placeholdit.imgix.net/~text?txtsize=30&bg=fff&w=240&h=160&txt=Thumbnail" alt="{{ $post->name }}">
-        </a>
-    </div>
-    <div class="col-xs-8">
-        <h2 class="h4">{!! link_to_route('post.show', $post->name, $post) !!}</h2>
-        <ul class="list-inline">
-            <li>{{ trans('post.author') }}: {{ $post->author->name }}</li>
-            <li>{{ trans('post.date') }}:
-                <time datetime="{{ $post->created_at->toDateTimeString() }}">{{ $post->created_at->toDateTimeString() }}</time>
-            </li>
-        </ul>
-        <p>{{ $post->description }}</p>
-        <div class="text-right">
-            {!! link_to_route('post.show', trans('post.more'), $post, ['class' => 'btn btn-primary']) !!}
+<article class="panel post-item">
+    <div class="panel-body row">
+        <div class="col-xs-4">
+            <a href="{{ route('post.show', $post) }}">
+                <img class="img-thumbnail" src="https://placeholdit.imgix.net/~text?txtsize=30&bg=fff&w=240&h=160&txt=Thumbnail" alt="{{ $post->name }}">
+            </a>
         </div>
+        <div class="col-xs-8">
+            <h2 class="h4">{!! link_to_route('post.show', $post->name, $post) !!}</h2>
+            <ul class="list-inline">
+                <li>{{ trans('post.author') }}: {{ $post->author->name }}</li>
+                <li>{{ trans('post.date') }}:
+                    <time datetime="{{ $post->created_at->toDateTimeString() }}">{{ $post->created_at->toDateTimeString() }}</time>
+                </li>
+            </ul>
+            <p>{{ $post->description }}</p>
+        </div>
+    </div>
+    <div class="panel-footer text-right">
+        {!! link_to_route('post.show', trans('post.more'), $post, ['class' => 'btn btn-primary']) !!}
     </div>
 </article>
